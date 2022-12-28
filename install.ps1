12e9715e99c5e2dda7585e27e3e9b51045f937d7
Peter Siemens <peterjsiemens@gmail.com>
2022-12-27 22:21:09 -0800

    Remove version parameter from Windows install script, install from git

--- install.ps1
+++ install.ps1
@@ -3,22 +3,15 @@
     Install the Stakeout CLI on Windows.
 .DESCRIPTION
     By default, the latest release will be installed.
-    If '-Version' is specified, then the given version is installed.
 .Parameter Directory
     The destination path to install to.
-.Parameter Version
-    The version to install.
 .Parameter AddToPath
     Add the absolute destination path to the 'User' scope environment variable 'Path'.
 .EXAMPLE
     Install the current version
     .\install.ps1
-.EXAMPLE
-    Install version v0.5.2
-    .\install.ps1 -Version v0.5.2
 #>
 param (
-    [string] $version="",
     [string] $directory = "$env:APPDATA\Stakeout",
     [bool] $addToPath = $true
 )
@@ -30,26 +23,15 @@ Set-ItemProperty HKCU:\Console VirtualTerminalLevel -Type DWORD 1
 
 $ErrorActionPreference = "Stop"
 
-$repo = "psiemens/stakeout"
-$versionURL = "https://api.github.com/repos/$repo/releases/latest"
-$assetsURL = "https://github.com/$repo/releases/download"
+$baseURL = "https://raw.githubusercontent.com/psiemens/stakeout/main"
 
-if (!$version) {
-    $q = (Invoke-WebRequest -Uri "$versionURL" -UseBasicParsing) | ConvertFrom-Json
-    $version = $q.tag_name
-}
-
-Write-Output("Installing version {0} ..." -f $version)
+Write-Output("Installing Stakeout...")
 
 New-Item -ItemType Directory -Force -Path $directory | Out-Null
 
 $progressPreference = 'silentlyContinue'
 
-Invoke-WebRequest -Uri "$assetsURL/$version/stakeout-x86_64-windows.zip" -UseBasicParsing -OutFile "$directory\stakeout.zip"
-
-Expand-Archive -Path "$directory\stakeout.zip" -DestinationPath "$directory"
-
-Move-Item -Path "$directory\stakeout.exe" -Destination "$directory\stakeout.exe" -Force
+Invoke-WebRequest -Uri "$baseURL/stakeout-x86_64-windows" -UseBasicParsing -OutFile "$directory\stakeout.exe"
 
 if ($addToPath) {
     Write-Output "Adding to PATH ..."
